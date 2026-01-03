a1179ff796f93f82f8d9e2ba5092f95e9f004f5f
jebb <gabouchard2002@gmail.com>
2026-01-03 12:05:07 +0300

    add new endpoint

--- src/endpoints/users.py
+++ src/endpoints/users.py
@@ -1,7 +1,7 @@
 import json
 import logging
 from json import loads
-from typing import Any, Union
+from typing import Any, Union, Dict
 
 import httpx
 from fastapi import APIRouter
@@ -22,20 +22,40 @@ redis = aioredis.from_url("redis://redis")
 router = APIRouter(prefix='/api')
 
 
+@router.get('/check_profile')
+async def check_profile_exists(
+        user_id: int = Query(..., description="User ID")
+) -> dict[str, Any]:
+    async with httpx.AsyncClient() as client:
+
+        url = DATABASE_BASE_URL + config.database.prefix + \
+              f'/profile_exists?user_id={user_id}'
+
+        resp = await client.get(url=url)
+        if resp.status_code == 200:
+            profile_exists = resp.json()
+            logger.info(f'profile exists: {profile_exists}')
+            return { 'exists': profile_exists }
+
+        raise HTTPException(status_code=resp.status_code, detail=resp.text)
+
 
 @router.get("/nicknames")
 async def check_nickname_exists(
         nickname: str = Query(..., description="Some user`s nickname")
 ) -> bool:
     async with httpx.AsyncClient() as client:
+
         url = DATABASE_BASE_URL + config.database.prefix + \
               f'/nickname_exists?nickname={nickname}'
-        response = await client.get(url=url, timeout=5.0)
-        if response.status_code == 200:
-            nickname_exists = response.json()
+
+        resp = await client.get(url=url, timeout=5.0)
+        if resp.status_code == 200:
+            nickname_exists = resp.json()
             logger.info(f'nickname exists: {nickname_exists}')
             return nickname_exists
-        raise HTTPException(status_code=500, detail="Error connecting to server")
+
+        raise HTTPException(status_code=resp.status_code, detail=resp.text)
 
 
 @router.get("/users")
@@ -48,10 +68,10 @@ async def get_user_via_gateway(
         async with (httpx.AsyncClient() as client):
             url = DATABASE_BASE_URL + config.database.prefix + \
                 f'/user_exists?user_id={user_id}'
-            response = await client.get(url=url, timeout=5.0)
-            if response.status_code == 200:
-                return response.json()
-            raise HTTPException(status_code=500, detail="Error connecting to server")
+            resp = await client.get(url=url, timeout=5.0)
+            if resp.status_code == 200:
+                return resp.json()
+            raise HTTPException(status_code=resp.status_code, detail=resp.text)
 
     cached = await redis.hgetall(f'user:{user_id}:{target_field}')
     if cached:
@@ -61,20 +81,21 @@ async def get_user_via_gateway(
         async with httpx.AsyncClient() as client:
             url = DATABASE_BASE_URL + config.database.prefix + \
                   f"/users?user_id={user_id}&target_field={target_field}"
-            response = await client.get(
+            resp = await client.get(
                 url=url,
                 timeout=5.0
             )
-            if response.status_code == 200:
-                data = response.json()
+            if resp.status_code == 200:
+                data = resp.json()
                 mapping = {key: json.dumps(value) for key, value in data.items()}
                 await redis.hset(f'user:{user_id}:{target_field}', mapping=mapping)
-                return response
+                return resp
 
             return None
 
     except Exception as e:
         logger.error(f'Failed to redirect request: {e}')
+        raise HTTPException(status_code=resp.status_code, detail=resp.text)
 
 
 @router.post("/users")
@@ -107,7 +128,8 @@ async def create_user_via_gateway(user_data: User):
             return {"status": "success"}
 
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Failed to update DB: {e}")
+        logger.error(f"Failed to update DB: {e}")
+        raise HTTPException(status_code=resp.status_code, detail=resp.text)
 
 
 @router.put('/update_profile')
@@ -142,4 +164,5 @@ async def update_user_profile(
                 await redis.delete(f'user:{updated_data.user_id}:profiles')
 
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error updating data: {e}")
+        logger.error(f"Failed to update DB: {e}")
+        raise HTTPException(status_code=resp.status_code, detail=resp.text)
